142c40cb23c568de38b8b488b25f893cf377b583
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-10-27 21:01:18 +0100

    added function to add students

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('#studentForm');
 const input = document.querySelector('#name');
 const btn = document.querySelector('#submit');
+const studentList = document.querySelector('#students');
 
 input.addEventListener('keyup', function (e) {
     if (e.keyCode === 13) {
@@ -41,7 +42,23 @@ const addStudent = function () {
     let li = document.createElement('li');
 
     li.className = 'list-group-item d-flex justify-content-between';
-
+    li.innerHTML =
+        `
+        ${newStudent}
+<div>
+<div class="form-check form-check-inline">
+    <input type="checkbox" class="form-check-input" id="yes">
+    <label for="yes" class="form-check-label">Yes</label>
+</div>
+<div class="form-check form-check-inline">
+    <input type="checkbox" class="form-check-input" id="no">
+    <label for="no" class="form-check-label">No</label>
+</div>
+<div id="check-error"></div>
+</div>
+`
+    // let check = document.createElement('input')
+    studentList.appendChild(li);
 }
 
 form.addEventListener('submit', function (e) {
