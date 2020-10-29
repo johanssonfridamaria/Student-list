338b4524f567a93534d6db8607d0871572695542
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-10-29 08:41:19 +0100

    updated

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
+let students = [
+    {
+        date: '2020-10-29',
+        name: 'Frida Johansson',
+        attending: false
+    },
+    {
+        date: '2020-10-29',
+        name: 'Klara Johansson',
+        attending: false
+    },
+    {
+        date: '2020-10-29',
+        name: 'Arvid Johansson',
+        attending: false
+    },
+]
+
 const form = document.querySelector('#studentForm');
 const input = document.querySelector('#name');
 const btn = document.querySelector('#submit');
 const studentList = document.querySelector('#students');
+const check = document.querySelector('#attending');
+const date = new Date();
 
-input.addEventListener('keyup', function (e) {
+function enter(e) {
     if (e.keyCode === 13) {
         e.preventDefault();
-        form.submit();
     }
-});
-
+}
 
 const validate = function (id) {
     const input = document.querySelector(id);
@@ -36,34 +54,60 @@ const validate = function (id) {
     }
 }
 
-const addStudent = function () {
-    const newStudent = input.value;
-
-    let li = document.createElement('li');
-
-    li.className = 'list-group-item d-flex justify-content-between';
-    li.innerHTML =
-        `
-        ${newStudent}
-<div>
-<div class="form-check form-check-inline">
-    <input type="checkbox" class="form-check-input" id="yes">
-    <label for="yes" class="form-check-label">Yes</label>
-</div>
-<div class="form-check form-check-inline">
-    <input type="checkbox" class="form-check-input" id="no">
-    <label for="no" class="form-check-label">No</label>
-</div>
-<div id="check-error"></div>
-</div>
-`
-    // let check = document.createElement('input')
-    studentList.appendChild(li);
+const addStudent = () => {
+    
 }
+
+function attending(check) {
+    if (check.checked) {
+        return true
+    }
+    else
+        return false
+}
+
+function listStudents() {
+    studentList.innerHTML='';
+    students.forEach(student=>{
+        let li = document.createElement('li');
+        li.className = 'list-group-item d-flex justify-content-between';
+        li.innerHTML =
+            `
+            <div class="pl-3">
+            <input type="checkbox" class="form-check-input" id="attend">
+            <label for="attend" class="form-check-label"></label>
+        </div>
+        ${student.name}
+        <button class="btn btn-danger px-3">X</button>
+        `
+        studentList.appendChild(li);
+    })
+    localStorage.setItem("students", JSON.stringify(students));
+}
+
+let old = JSON.parse(localStorage.getItem("students"));
+
+if(old !=null){
+    students=old;
+}
+
+input.addEventListener('keyup', enter);
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     if (validate('#name')) {
-        addStudent();
+      
+
+        // checkValidation('#yes', '#no');
+        let student = {
+            date: date,
+            name: e.currentTarget.name.value,
+            attending: false
+        }
+        students.push(student);
+        listStudents();
+        input.value='';
     }
 })
+
+listStudents();
